2aa10878efd7af2c832de10835a882079f1e984e
Yuval Mirski <mirskiyuval7@gmail.com>
2022-01-03 15:07:34 +0200

    blabla

---
+++ app/src/main/java/com/example/travel_guide/model/UserDao.java
@@ -0,0 +1,21 @@
+package com.example.travel_guide.model;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserPostDao {
+    @Query("select * from UserPost")
+    List<UserPost> getAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(UserPost... userPosts);
+
+    @Delete
+    void delete(UserPost userPost) ;
+}
