cfd5ec4b214dfb332804dab8f7970fd7825dcda7
eli <eliyahu280@gmail.com>
2022-01-10 14:54:55 +0200

    update

---
+++ app/src/main/java/com/example/travel_guide/NewPostPageViewModel.java
@@ -0,0 +1,25 @@
+package com.example.travel_guide;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.travel_guide.model.Model;
+import com.example.travel_guide.model.User;
+
+public class NewPostPageViewModel extends ViewModel {
+
+
+    LiveData<User> userLiveData;
+    String userId;
+    public NewPostPageViewModel() {
+
+    }
+    public void updateUser(String userId){
+        this.userId = userId;
+        userLiveData = Model.instance.getUser(userId);
+    }
+    public LiveData<User> getUserLiveData() {
+        return userLiveData;
+    }
+
+}
